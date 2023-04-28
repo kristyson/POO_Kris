8a85b268dd55e8a560fff45309bbe2b57f2d99ad
kristyson <kristyson.business@gmail.com>
2023-04-28 16:25:36 -0300

    Idioma

---
+++ POO_Kris/java/Basico/Exercicios/IdentificadorDeIdioma.java
@@ -0,0 +1,11 @@
+package POO_Kris.java.Basico.Exercicios;
+
+import java.util.Locale;
+
+public class IdentificadorDeIdioma {
+    public static void main(String[] args) {
+        Locale IdiomaLocal = Locale.getDefault();
+        
+        System.out.println("O Idioma local é > "+ IdiomaLocal);
+    }
+}
