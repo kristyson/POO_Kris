5ee7598e137bb6b25500d569e69a538fa3250376
kristyson <kristyson.business@gmail.com>
2023-04-28 09:52:44 -0300

    Organizando os diretórios

---
+++ POO_Kris/java/Basico/Hello_World.java
@@ -0,0 +1,7 @@
+package POO_Kris.java.Basico;
+
+public class Hello_World{
+    public static void main(String[] args) {
+        System.out.print ("Hello World");
+    }
+}
---
+++ POO_Kris/java/Basico/PrimeirosCodigos/BoletimEstudantil.java
@@ -0,0 +1,14 @@
+package POO_Kris.java.Basico.PrimeirosCodigos ;
+    public class BoletimEstudantil {
+        public static void main(String[]args){
+            int mediaFinal = 8;
+            
+            if (mediaFinal < 7 )
+                System.out.println("REPROVADO");
+            else if (mediaFinal == 7 ) 
+                System.out.println("RECUPERAÇÂO");
+            else
+                System.out.println("APROVADO") ;
+
+        }
+    }
