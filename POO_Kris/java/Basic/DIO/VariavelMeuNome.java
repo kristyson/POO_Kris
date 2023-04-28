d8b0d4b2e1d37cc273353c60ee537caab71fa1f0
kristyson <kristyson.business@gmail.com>
2023-04-28 09:08:36 -0300

    .

---
+++ POO_Kris/java/Basic/DIO/VariavelMeuNome.java
@@ -0,0 +1,18 @@
+package POO_Kris.java.Basic.DIO;
+
+public class VariavelMeuNome {
+
+    public static void main (String [] args) {
+        String primeiroNome = "Kristyson" ;
+        String segundoNome = "Alpino" ;
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome) ;
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do metodo " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+    
+}
