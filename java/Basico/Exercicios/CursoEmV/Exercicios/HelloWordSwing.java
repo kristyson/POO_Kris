164f188a1ceabf1b88380787444571e1b5b98bdd
kristyson <kristyson.business@gmail.com>
2023-04-30 13:40:53 -0300

    .

---
+++ java/Basico/Exercicios/CursoEmV/Exercicios/HelloWordSwing.java
@@ -0,0 +1,15 @@
+package Exercicios;
+
+import javax.swing.JFrame;
+
+public class HelloWordSwing {   
+
+    public static void main(String[] args) {
+         
+        JFrame janela = new JFrame("Hello World");
+        janela.setSize(500,500);
+        janela.setVisible(true);
+        
+
+    }
+}
